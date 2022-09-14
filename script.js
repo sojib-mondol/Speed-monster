9e71f13908e4d0fc21d956b32d141b44e8c97a59
sojib-sudo <sojibmandol@gmail.com>
2022-09-15 01:03:57 +0600

    3 column done & some fractional value done

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML += `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${timeTaken.toFixed(2)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
